3a4557cb6ff5367eb81283ac946e9a6b921da756
PatricioHenderson <patriciohenderson@hotmail.com>
2021-07-31 07:28:46 -0300

    wish list creada

--- marvel/e_commerce/admin.py
+++ marvel/e_commerce/admin.py
@@ -1,34 +1,41 @@
 from django.contrib import admin
 
 # NOTE: Tenemos que importar los modelos con los que vamos a trabajar:
-# from e_commerce.models import *
+from e_commerce.models import *
 
 # Register your models here.
 
 # NOTE: Aquí personalizamos los campos en el Django Admin.
 
-# @admin.register(Comic)
-# class ComicsAdmin(admin.ModelAdmin):
-    # NOTE: Para seleccionar los campos en la tabla de registros
-    # list_display = ('marvel_id', 'title', 'stock_qty', 'price')
+@admin.register(Comic)
+class ComicsAdmin(admin.ModelAdmin):
+    #NOTE: Para seleccionar los campos en la tabla de registros
+    list_display = ('marvel_id', 'title', 'stock_qty', 'price')
 
-    # NOTE: Filtro lateral de elementos:
-    # list_filter= ('marvel_id','title')
+    #NOTE: Filtro lateral de elementos:
+    list_filter= ('marvel_id','title')
     
-    # NOTE: Buscador de elementos en la columna:
-    # search_fields = ['title']
+    #NOTE: Buscador de elementos en la columna:
+    search_fields = ['title']
 
-    # NOTE: Para seleccionar los campos en el registro. 
-    # fields = ('marvel_id', 'title', 'stock_qty')
+    #NOTE: Para seleccionar los campos en el registro. 
+    #fields = ('marvel_id', 'title', 'stock_qty')
 
-    # NOTE: Genera un campo desplegable con los registros seleccionados.
-    # fieldsets = (
-    #     (None, {
-    #         'fields': ('marvel_id', 'title', 'stock_qty')
-    #     }),
-    #     ('Advanced options', {
-    #         'classes': ('collapse',),
-    #         'fields': ('description','price', 'picture'),
-    #     }),
-    # )
-    # pass
+    #NOTE: Genera un campo desplegable con los registros seleccionados.
+    fieldsets = (
+        (None, {
+            'fields': ('marvel_id', 'title', 'stock_qty')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('description','price', 'picture'),
+        }),
+    )
+    pass
+
+@admin.register(Wish_list)
+class Wish_listsAdmin(admin.ModelAdmin):
+    fields = (  'user_id' ,'comic_id' , 'favorite' , 'cart' ,  'wished_qty' , 'buyed_qty')
+
+    list_display = ( 'user_id' ,'comic_id' , 'favorite' , 'cart' , 'wished_qty' , 'buyed_qty')
+    pass
--- marvel/e_commerce/models.py
+++ marvel/e_commerce/models.py
@@ -3,39 +3,68 @@ from django.db import models
 # NOTE: Para poder utilizar el modelo "user" que viene por defecto en Django,
 # Debemos importarlo previamente:
 
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
+from django.db.models.deletion import DO_NOTHING
 
 # Create your models here.
 
 
-# class Comic(models.Model):
-#     '''
-#     Esta clase hereda de Django models.Model y crea una tabla llamada
-#     e_commerce_comic. Las columnas toman el nombre especificado de cada objeto.
-#     '''
-#     id = models.BigAutoField(db_column='ID', primary_key=True)
-#     marvel_id = models.PositiveIntegerField(
-#         verbose_name='marvel ids', default=1, unique=True)
-#     title = models.CharField(
-#         verbose_name='titles', max_length=120, default='')
-#     description = models.TextField(
-#         verbose_name='descriptions', default='')
-#     price = models.FloatField(
-#         verbose_name='prices', max_length=5, default=0.00)
-#     stock_qty = models.PositiveIntegerField(
-#         verbose_name='stock qty', default=0)
-#     picture = models.URLField(
-#         verbose_name='pictures', default='')
+class Comic(models.Model):
+    '''
+    Esta clase hereda de Django models.Model y crea una tabla llamada
+    e_commerce_comic. Las columnas toman el nombre especificado de cada objeto.
+    '''
+    id = models.BigAutoField(db_column='ID', primary_key=True)
+    marvel_id = models.PositiveIntegerField(
+        verbose_name='marvel ids', default=1, unique=True)
+    title = models.CharField(
+        verbose_name='titles', max_length=120, default='')
+    description = models.TextField(
+        verbose_name='descriptions', default='')
+    price = models.FloatField(
+        verbose_name='prices', max_length=5, default=0.00)
+    stock_qty = models.PositiveIntegerField(
+        verbose_name='stock qty', default=0)
+    picture = models.URLField(
+        verbose_name='pictures', default='')
 
-#     class Meta:
-#         '''
-#         Con "class Meta" podemos definir atributos de nuestras entidades como el nombre de la tabla.
-#         '''
-#         db_table = 'e_commerce_comics'
+    class Meta:
+        '''
+        Con "class Meta" podemos definir atributos de nuestras entidades como el nombre de la tabla.
+        '''
+        db_table = 'e_commerce_comics'
 
-#     def __str__(self):
-#         '''
-#         La función __str__ cumple la misma función que __repr__ en SQL Alchemy, 
-#         es lo que retorna cuando llamamos al objeto.
-#         '''
-#         return f'{self.id}'
+    def __str__(self):
+        '''
+        La función __str__ cumple la misma función que __repr__ en SQL Alchemy, 
+        es lo que retorna cuando llamamos al objeto.
+        '''
+        return f'{self.title}'
+
+class Wish_list(models.Model):
+
+    id = models.BigAutoField(db_column='ID', primary_key=True)
+
+    user_id = models.ForeignKey(
+        User, on_delete=models.DO_NOTHING , verbose_name='User ID' , blank=True , default=1)
+    
+    comic_id = models.ForeignKey(
+        Comic, on_delete=DO_NOTHING, verbose_name='Comic', default=1)
+
+    favorite = models.BooleanField(
+        verbose_name='Favorite' , default=1)
+
+    cart = models.PositiveIntegerField(
+        verbose_name='Cart' , default=0)
+
+    wished_qty = models.PositiveIntegerField(
+        verbose_name='Wished quantity',default=0)
+
+    buyed_qty = models.PositiveIntegerField(
+        verbose_name='Buyed quantity', default=0)
+
+    class Meta:
+        db_table = 'e_commerce_Wish_list'
+        # ordering = [user_id]
+    def __str__(self):
+        return f'{self.user_id}'
